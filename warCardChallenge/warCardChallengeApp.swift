becdf56b7e11778c849c08b6399fc7ea8d8d1bf9
Ahmad Dorra <ahmadeldorra1@gmail.com>
2023-04-09 21:14:43 +0300

    Initial Commit

---
+++ warCardChallenge/warCardChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  warCardChallengeApp.swift
+//  warCardChallenge
+//
+//  Created by Ahmad Dorra on 09/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct warCardChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
